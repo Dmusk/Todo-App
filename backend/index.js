9a81fd7730be07414794f44a32e7e8e4932a0ad7
Dmusk <aadarsh.wadile@mitaoe.ac.in>
2024-07-10 23:08:09 +0530

    added the code for app.ge and app.completed

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,21 @@
 const express = require("express");
+const { createTodo, updateTodo } = require("./types");
 
 const app = express();
 
 app.use(express.json());
 
 app.post("/todo", (req, res) => {
-  res.send("Hello Their");
+  const createpayload = req.body;
+  const parsepayload = createTodo.safeParse(createpayload);
+
+  if (!parsepayload.success) {
+    res.status(403).json({
+      msg: "Invalid input",
+    });
+    return;
+  }
+  //else put it in mongo DB;
 });
 
 app.get("/todos", (req, res) => {
@@ -13,7 +23,16 @@ app.get("/todos", (req, res) => {
 });
 
 app.put("/completed", (req, res) => {
-  res.send("Hello Their");
+  const createpayload = req.body;
+  const parsepayload = updateTodo.safeParse(createpayload);
+
+  if (!parsepayload.success) {
+    res.status(403).json({
+      msg: "Invalid Inputs",
+    });
+    return;
+  }
+  //else
 });
 
 app.listen(4500);
