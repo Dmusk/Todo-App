c691a430d55bb64cb6d85b1df26e2cbf7e52a7c4
Dmusk <aadarsh.wadile@mitaoe.ac.in>
2024-07-11 09:30:33 +0530

    completed teh post and ut req

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
+const { todo } = require("./db");
 
 const app = express();
 
 app.use(express.json());
 
-app.post("/todo", (req, res) => {
+app.post("/todo", async (req, res) => {
   const createpayload = req.body;
   const parsepayload = createTodo.safeParse(createpayload);
 
@@ -16,13 +17,25 @@ app.post("/todo", (req, res) => {
     return;
   }
   //else put it in mongo DB;
+  await todo.create({
+    title: createpayload.title,
+    description: createpayload.description,
+    completed: false,
+  });
+
+  res.json({
+    msg: "Added Task Successfully",
+  });
 });
 
 app.get("/todos", (req, res) => {
-  res.send("Hello Their");
+  const todos = todo.find({});
+  res.json({
+    todos,
+  });
 });
 
-app.put("/completed", (req, res) => {
+app.put("/completed", async (req, res) => {
   const createpayload = req.body;
   const parsepayload = updateTodo.safeParse(createpayload);
 
@@ -33,6 +46,18 @@ app.put("/completed", (req, res) => {
     return;
   }
   //else
+  await todo.update(
+    {
+      id: req.body.id, //search for the stuff need to be updated
+    },
+    {
+      completed: true, //update what to update
+    }
+  );
+
+  res.json({
+    msg: "Mark As Completed",
+  });
 });
 
 app.listen(4500);
